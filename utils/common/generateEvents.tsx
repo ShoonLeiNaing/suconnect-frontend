f2f411155cc7b0dde6a6ee69f975375d200599e9
ShoonLeiNaing <pandapanda254.pp@gmail.com>
2022-11-03 10:53:15 +0630

    holidays added in calendar

--- utils/common/generateEvents.tsx
+++ utils/common/generateEvents.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable no-nested-ternary */
+
 import moment from "moment";
+import { AiFillHome } from "react-icons/ai";
+import { FaGraduationCap } from "react-icons/fa";
 
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 export const generateEvents = (events: any) => {
@@ -6,16 +10,27 @@ export const generateEvents = (events: any) => {
     const date = event.date.split("-");
     const startTime = event.time_from.split(":");
     const endTime = event.time_to.split(":");
-    const eventType = event.classification === 10 && "Lecture Day";
+    const eventType =
+      event.classification === 10
+        ? "Lecture Day"
+        : event.classification === 9
+        ? "Holiday"
+        : "Others";
     return {
       id: event.id,
       day: daysOfWeek[
         moment(new Date(date[0], date[1], date[2])).subtract(1, "months").day()
       ],
+      icon:
+        event?.classification === 10 ? (
+          <FaGraduationCap fontSize="16px" />
+        ) : (
+          <AiFillHome fontSize="16px" />
+        ),
       classification: event.classification,
       course: event.course,
-      title: `${eventType} ${index + 1}`,
-      color: "pink",
+      title: `${eventType} ${event.classification === 10 ? index + 1 : ""}`,
+      color: event.classification === 10 ? "pink" : "blue",
       startDate: new Date(
         date[0],
         date[1] - 1,
